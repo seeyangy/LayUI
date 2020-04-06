72b8393b78a4bd6f5c99e4bd1d46332d5e00165e
杨源 <seeyangy@qq.com>
2020-04-06 21:15:15 +0800

    v6

--- 1.2.1/layuiadmin/config.js
+++ 1.2.1/layuiadmin/config.js
@@ -12,14 +12,14 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function (exports) {
     container: 'LAY_app' //容器ID
     , base: layui.cache.base //记录layuiAdmin文件夹所在路径
     , views: '/' //视图所在目录
-    , entry: layui.cache.appname +'/frontpage' //默认视图文件名
-    , engine: '.html' //视图文件后缀名
+    , entry: layui.cache.appname +'/frontpage.html' //默认视图文件名
+    , engine: '' //视图文件后缀名
     , pageTabs: $.cookie("pagemode") === 'Tab' //是否开启页面选项卡功能。单页版不推荐开启
     , name: 'OLK'
     , tableName: 'layuiAdmin' //本地存储表名
     , MOD_NAME: '_olk' //模块事件名
     , debug: true //是否开启调试模式。如开启，接口异常时会抛出异常 URL 等信息
-    , interceptor: false //是否开启未登入拦截
+    , interceptor: true //是否开启未登入拦截
     //自定义请求字段
     , request: {
       tokenName: 'access_token' //自动携带 token 的字段名。可设置 false 不携带。
@@ -35,10 +35,10 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function (exports) {
       , dataName: 'data' //数据详情的字段名称
     }
     //独立页面路由，可随意添加（无需写参数）
-    , indPage: ['/user/login' //登入页
-      , '/user/reg' //注册页
-      , '/user/forget' //找回密码
-      , '/template/tips/test' //独立页的一个测试 demo
+    , indPage: [layui.cache.appname+'/user/login.html' //登入页
+      , '/user/reg.html' //注册页
+      , '/user/forget.html' //找回密码
+      , '/template/tips/test.html' //独立页的一个测试 demo
     ]
 
     //扩展的第三方模块
